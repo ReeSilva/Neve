35a1f5bce007e0e396f0785282e54868f74252ee
Renato Biancalana da Silva <renato.biancalana@taxfix.de>
2024-12-08 23:26:33 +0100

    some tests

--- config/ui/default.nix
+++ config/ui/default.nix
@@ -23,7 +23,7 @@
     barbecue.enable = lib.mkDefault true;
     dressing-nvim.enable = lib.mkDefault true;
     indent-blankline.enable = lib.mkDefault true;
-    noice.enable = lib.mkDefault false;
+    noice.enable = lib.mkDefault true;
     notify.enable = lib.mkDefault true;
     nui.enable = lib.mkDefault true;
     web-devicons.enable = lib.mkDefault true;
