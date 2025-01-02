151ba63ad356406100d94f638bc1d7cb07172b57
Renato Biancalana da Silva <renato.biancalana@taxfix.de>
2025-01-02 15:46:19 +0100

    fix: set capabilities for lsp
    this commit fix an error that appears everytime we open an yaml file,
    caused by a problem between yaml-language-server and nvim-ufo
    See:
    https://github.com/redhat-developer/yaml-language-server/issues/912
    https://github.com/kevinhwang91/nvim-ufo/tree/main#minimal-configuration

--- config/sets/set.nix
+++ config/sets/set.nix
@@ -119,8 +119,19 @@
       local opt = vim.opt
       local g = vim.g
       local o = vim.o
-      g.disable_autoformat = true
-        -- Neovide
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.textDocument.foldingRange = {
+        dynamicRegistration = false,
+        lineFoldingOnly = true
+      }
+      local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+      for _, ls in ipairs(language_servers) do
+          require('lspconfig')[ls].setup({
+              capabilities = capabilities
+              -- you can add other fields for setting up lsp server in this table
+          })
+      end
+      -- Neovide
       if g.neovide then
         g.neovide_fullscreen = false
         g.neovide_hide_mouse_when_typing = false
