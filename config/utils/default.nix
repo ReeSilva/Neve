3f8f97baea4a3bb7af6e9932a7d67b9d95d85dc5
reesilva <renato.biancalana@taxfix.de>
2025-05-23 12:18:34 +0200

    remove harpoon for now

--- config/utils/default.nix
+++ config/utils/default.nix
@@ -3,7 +3,7 @@
     ./avante.nix
     ./better-escape.nix
     ./cloak.nix
-    ./harpoon.nix
+    # ./harpoon.nix
     ./markdown-preview.nix
     ./mini.nix
     ./neocord.nix
@@ -32,7 +32,7 @@
   config = lib.mkIf config.utils.enable {
     better-escape.enable = lib.mkDefault true;
     cloak.enable = lib.mkDefault true;
-    harpoon.enable = lib.mkDefault true;
+    # harpoon.enable = lib.mkDefault true;
     markdown-preview.enable = lib.mkDefault true;
     mini.enable = lib.mkDefault true;
     neocord.enable = lib.mkDefault true;
