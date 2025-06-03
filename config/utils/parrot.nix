7d8e11ee6c793835c3831998a7d481aaaef79501
reesilva <renato.biancalana@taxfix.de>
2025-06-03 16:37:54 +0200

    trying fixes, i need

--- config/utils/parrot.nix
+++ config/utils/parrot.nix
@@ -9,15 +9,15 @@
           perplexity = {
             name = "perplexity";
             endpoint = "https://api.perplexity.ai/chat/completions";
-            api_key = lib.nixvim.utils.mkRaw "os.getenv 'PERPLEXITY_API_KEY'";
-            models = {
-              __unkeyed-1 = "llama-3.1-sonar-small-128k-chat";
-              __unkeyed-2 = "llama-3.1-sonar-small-128k-online";
-              __unkeyed-3 = "llama-3.1-sonar-large-128k-chat";
-              __unkeyed-4 = "llama-3.1-sonar-large-128k-online";
-              __unkeyed-5 = "llama-3.1-8b-instruct";
-              __unkeyed-6 = "llama-3.1-70b-instruct";
-            };
+            api_key = lib.nixvim.utils.mkRaw "os.getenv('PERPLEXITY_API_KEY')";
+            # models = {
+            #   __unkeyed-1 = "llama-3.1-sonar-small-128k-chat";
+            #   __unkeyed-2 = "llama-3.1-sonar-small-128k-online";
+            #   __unkeyed-3 = "llama-3.1-sonar-large-128k-chat";
+            #   __unkeyed-4 = "llama-3.1-sonar-large-128k-online";
+            #   __unkeyed-5 = "llama-3.1-8b-instruct";
+            #   __unkeyed-6 = "llama-3.1-70b-instruct";
+            # };
           };
         };
       };
