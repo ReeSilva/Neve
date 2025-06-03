08cc6d8ac2572caaa459ccd8105054376247c02d
reesilva <renato.biancalana@taxfix.de>
2025-06-03 17:15:53 +0200

    here it goes?

--- config/utils/parrot.nix
+++ config/utils/parrot.nix
@@ -10,15 +10,28 @@
             name = "perplexity";
             endpoint = "https://api.perplexity.ai/chat/completions";
             api_key = lib.nixvim.utils.mkRaw "os.getenv('PERPLEXITY_API_KEY')";
-            topic = { model = "llama-3.1-70b-instruct"; };
-            # models = {
-            #   __unkeyed-1 = "llama-3.1-sonar-small-128k-chat";
-            #   __unkeyed-2 = "llama-3.1-sonar-small-128k-online";
-            #   __unkeyed-3 = "llama-3.1-sonar-large-128k-chat";
-            #   __unkeyed-4 = "llama-3.1-sonar-large-128k-online";
-            #   __unkeyed-5 = "llama-3.1-8b-instruct";
-            #   __unkeyed-6 = "llama-3.1-70b-instruct";
-            # };
+            params = {
+              chat = {
+                temperature = 1.1;
+                top_p = 1;
+              };
+              command = {
+                temperature = 1.1;
+                top_p = 1;
+              };
+            };
+            topic = {
+              model = "llama-3.1-70b-instruct";
+              params = { max_tokens = 64; };
+            };
+            models = [
+              "llama-3.1-sonar-small-128k-chat"
+              "llama-3.1-sonar-small-128k-online"
+              "llama-3.1-sonar-large-128k-chat"
+              "llama-3.1-sonar-large-128k-online"
+              "llama-3.1-8b-instruct"
+              "llama-3.1-70b-instruct"
+            ];
           };
         };
       };
