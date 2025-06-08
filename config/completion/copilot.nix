ccb46e3ebe7ad366576ca1a76bdf584c7ee5d67d
reesilva <reesilva@proton.me>
2025-06-08 19:43:30 +0200

    feat: copilot-cmp shananigans

--- config/completion/copilot.nix
+++ config/completion/copilot.nix
@@ -4,34 +4,10 @@
     plugins.copilot-lua = {
       enable = true;
       settings = {
-        panel = {
-          enabled = true;
-          auto_refresh = true;
-          keymap = {
-            jump_prev = "[[";
-            jump_next = "]]";
-            accept = "<CR>";
-            refresh = "gr";
-            open = "<M-CR>";
-          };
-          layout = {
-            position = "bottom"; # | top | left | right
-            ratio = 0.4;
-          };
-        };
-        suggestion = {
-          enabled = true;
-          auto_trigger = false;
-          debounce = 75;
-          keymap = {
-            accept = "<M-l>";
-            accept_word = false;
-            accept_line = false;
-            next = "<M-]>";
-            prev = "<M-[>";
-            dismiss = "<C-]>";
-          };
-        };
+        # Disabled for copilot-cmp
+        panel.enabled = false;
+        suggestion.enabled = false;
+
         filetypes = {
           yaml = false;
           markdown = false;
