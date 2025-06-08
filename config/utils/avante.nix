1bae9cf0fd415131c955cd6f083a9f16b9f235f1
reesilva <reesilva@proton.me>
2025-06-08 18:16:59 +0200

    feat: update avante config

--- config/utils/avante.nix
+++ config/utils/avante.nix
@@ -6,14 +6,8 @@
       enable = true;
       settings = {
         provider = "openai";
-        openai = {
-          api_key_name = "PERPLEXITY_API_KEY";
-          endpoint = "https://api.perplexity.ai";
-          model = "llama-3.1-sonar-large-128k-online";
-        };
         providers = [{
-          perplexity = {
-            __inherited_from = "openai";
+          openai = {
             api_key_name = "PERPLEXITY_API_KEY";
             endpoint = "https://api.perplexity.ai";
             model = "sonar-reasoning-pro";
