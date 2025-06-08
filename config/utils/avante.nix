453e5bbf67fc2126cffed6ccdf547beb259bcac3
reesilva <reesilva@proton.me>
2025-06-08 18:28:30 +0200

    feat: test some shit

--- config/utils/avante.nix
+++ config/utils/avante.nix
@@ -5,19 +5,20 @@
     plugins.avante = {
       enable = true;
       settings = {
-        provider = "openai";
-        openai = {
-          api_key_name = "PERPLEXITY_API_KEY";
-          endpoint = "https://api.perplexity.ai";
-          model = "llama-3.1-sonar-large-128k-online";
-        };
-        providers = [{
-          openai = {
+        provider = "perplexity";
+        # openai = {
+        #   api_key_name = "PERPLEXITY_API_KEY";
+        #   endpoint = "https://api.perplexity.ai";
+        #   model = "llama-3.1-sonar-large-128k-online";
+        # };
+        providers = {
+          perplexity = {
+            __inherited_from = "openai";
             api_key_name = "PERPLEXITY_API_KEY";
             endpoint = "https://api.perplexity.ai";
             model = "sonar-reasoning-pro";
           };
-        }];
+        };
       };
     };
   };
