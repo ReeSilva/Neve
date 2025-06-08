3e5b83faaba265106b50dc6a8173f2936b8bb24e
reesilva <reesilva@proton.me>
2025-06-08 18:21:44 +0200

    feat: back to old

--- config/utils/avante.nix
+++ config/utils/avante.nix
@@ -6,6 +6,11 @@
       enable = true;
       settings = {
         provider = "openai";
+        openai = {
+          api_key_name = "PERPLEXITY_API_KEY";
+          endpoint = "https://api.perplexity.ai";
+          model = "llama-3.1-sonar-large-128k-online";
+        };
         providers = [{
           openai = {
             api_key_name = "PERPLEXITY_API_KEY";
