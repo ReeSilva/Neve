8b4edccf04e9e9bc799139b157c1f73db9dd9bac
reesilva <reesilva@proton.me>
2025-06-26 22:15:23 +0200

    feat: some improvmentes for layzgit and avante

--- config/git/lazygit.nix
+++ config/git/lazygit.nix
@@ -1,27 +1,24 @@
-{ lib, config, ... }:
-{
-  options = {
-    lazygit.enable = lib.mkEnableOption "Enable lazygit module";
-  };
+{ lib, config, ... }: {
+  options = { lazygit.enable = lib.mkEnableOption "Enable lazygit module"; };
   config = lib.mkIf config.lazygit.enable {
 
     plugins.lazygit = {
       enable = true;
+      settings = {
+        floating_window_use_plenary = 1;
+        floating_window_winblend = 60;
+      };
     };
 
     extraConfigLua = ''
       require("telescope").load_extension("lazygit")
     '';
 
-    keymaps = [
-      {
-        mode = "n";
-        key = "<leader>gg";
-        action = "<cmd>LazyGit<CR>";
-        options = {
-          desc = "LazyGit (root dir)";
-        };
-      }
-    ];
+    keymaps = [{
+      mode = "n";
+      key = "<leader>gg";
+      action = "<cmd>LazyGit<CR>";
+      options = { desc = "LazyGit (root dir)"; };
+    }];
   };
 }
--- config/utils/avante.nix
+++ config/utils/avante.nix
@@ -1,20 +1,26 @@
-{ lib, config, pkgs, ... }: {
+{ lib, config, ... }: {
   options = { avante.enable = lib.mkEnableOption "Enable avante.nvim"; };
   config = let cfg = config.avante;
   in lib.mkIf cfg.enable {
-    plugins.avante = {
-      enable = true;
-      settings = {
-        provider = "copilot";
-        behaviour = { enable_cursor_planning_mode = true; };
-        providers = {
-          perplexity = {
-            __inherited_from = "openai";
-            api_key_name = "PERPLEXITY_API_KEY";
-            endpoint = "https://api.perplexity.ai";
-            model = "sonar-reasoning-pro";
+    plugins = {
+      render-markdown = {
+        enable = true;
+        settings = { file_types = [ "markdown" "Avante" ]; };
+      };
+      avante = {
+        enable = true;
+        settings = {
+          provider = "copilot";
+          behaviour = { enable_cursor_planning_mode = true; };
+          providers = {
+            perplexity = {
+              __inherited_from = "openai";
+              api_key_name = "PERPLEXITY_API_KEY";
+              endpoint = "https://api.perplexity.ai";
+              model = "sonar-reasoning-pro";
+            };
+            copilot = { model = "claude-sonnet-4"; };
           };
-          copilot = { model = "claude-sonnet-4"; };
         };
       };
     };
