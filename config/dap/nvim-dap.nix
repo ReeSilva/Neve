5046159d59e69558db0ded4383c33901a5f907f4
reesilva <reesilva@proton.me>
2025-09-09 12:43:51 +0200

    fix typo

--- config/dap/nvim-dap.nix
+++ config/dap/nvim-dap.nix
@@ -13,31 +13,28 @@
     };
     plugins.dap = {
       enable = true;
-      dap = {
-        enable = true;
-        signs = {
-          dapBreakpoint = {
-            text = "●";
-            texthl = "DapBreakpoint";
-          };
-          dapBreakpointCondition = {
-            text = "●";
-            texthl = "DapBreakpointCondition";
-          };
-          dapLogPoint = {
-            text = "◆";
-            texthl = "DapLogPoint";
-          };
+      signs = {
+        dapBreakpoint = {
+          text = "●";
+          texthl = "DapBreakpoint";
         };
-        configurations = {
-          java = [{
-            type = "java";
-            request = "launch";
-            name = "Debug (Attach) - Remote";
-            hostName = "127.0.0.1";
-            port = 5005;
-          }];
+        dapBreakpointCondition = {
+          text = "●";
+          texthl = "DapBreakpointCondition";
         };
+        dapLogPoint = {
+          text = "◆";
+          texthl = "DapLogPoint";
+        };
+      };
+      configurations = {
+        java = [{
+          type = "java";
+          request = "launch";
+          name = "Debug (Attach) - Remote";
+          hostName = "127.0.0.1";
+          port = 5005;
+        }];
       };
     };
     keymaps = [
