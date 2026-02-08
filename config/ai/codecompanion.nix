46bd1c192dd5b64fd1a6518be32ae17d726fba57
reesilva <renato.biancalana@taxfix.de>
2026-02-08 20:25:13 +0100

    feat: pimbas

--- config/ai/codecompanion.nix
+++ config/ai/codecompanion.nix
@@ -129,21 +129,21 @@
           };
         }
       ];
-      nixpkgs.overlays = [
-        (final: prev: {
-          vimPlugins = prev.vimPlugins // {
-            codecompanion-nvim = prev.vimPlugins.codecompanion-nvim.overrideAttrs {
-              version = "v18.5.0";
-              src = prev.fetchFromGitHub {
-                owner = "olimorris";
-                repo = "codecompanion.nvim";
-                tag = "v18.5.0";
-                sha256 = "sha256-UAvvj+iAWluJ2SD/KmyPovRjd/32351et/0Mftwehvw=";
-              };
-            };
-          };
-        })
-      ];
+      # nixpkgs.overlays = [
+      #   (final: prev: {
+      #     vimPlugins = prev.vimPlugins // {
+      #       codecompanion-nvim = prev.vimPlugins.codecompanion-nvim.overrideAttrs {
+      #         version = "v18.5.0";
+      #         src = prev.fetchFromGitHub {
+      #           owner = "olimorris";
+      #           repo = "codecompanion.nvim";
+      #           tag = "v18.5.0";
+      #           sha256 = "sha256-UAvvj+iAWluJ2SD/KmyPovRjd/32351et/0Mftwehvw=";
+      #         };
+      #       };
+      #     };
+      #   })
+      # ];
       plugins.codecompanion = {
         enable = true;
         lazyLoad.settings.cmd = [
--- config/completion/lspkind.nix
+++ config/completion/lspkind.nix
@@ -1,21 +1,6 @@
 { lib, config, ... }: {
   options = { lspkind.enable = lib.mkEnableOption "Enable lspkind module"; };
   config = lib.mkIf config.lspkind.enable {
-    nixpkgs.overlays = [
-      (final: prev: {
-        vimPlugins = prev.vimPlugins // {
-          lspkind-nvim = prev.vimPlugins.lspkind-nvim.overrideAttrs {
-            version = "0-unstable-2026-01-29";
-            src = prev.fetchFromGitHub {
-              owner = "onsails";
-              repo = "lspkind.nvim";
-              rev = "c7274c48137396526b59d86232eabcdc7fed8a32";
-              sha256 = "sha256-aIopYLm/x1CgCKpcsu9pxpqL0SXXhHDPTM8DKUwGeRw=";
-            };
-          };
-        };
-      })
-    ];
     plugins.lspkind = {
       enable = true;
       cmp.enable = false;
