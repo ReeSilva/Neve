fa1d4579fd0575f6349aadae4c6f15a44b9258cb
reesilva <renato.biancalana@taxfix.de>
2026-02-12 02:34:25 +0100

    feat: update

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
       inputs.nixpkgs.follows = "nixvim/nixpkgs";
     };
     opencode = {
-      url = "github:anomalyco/opencode?ref=v1.1.53";
+      url = "github:anomalyco/opencode?ref=v1.1.59";
     };
     pangaea = {
       url = "git+https://codeberg.org/reesilva/pangaea?ref=feat/v2";
