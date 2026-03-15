43df83ce5633d4d99a1aa9c409b83b8e86ac5e7b
reesilva <reesilva@proton.me>
2026-03-15 22:41:54 +0100

    feat: codelldb dap adapter for rust

--- config/languages/rust.nix
+++ config/languages/rust.nix
@@ -8,8 +8,13 @@
       enable = true;
       settings = {
         dap.adapter = {
-          type = "executable";
-          command = lib.getExe' inputs.rustacean.packages.${pkgs.stdenv.hostPlatform.system}.codelldb "codelldb";
+          type = "server";
+          host = "127.0.0.1";
+          port = "65534";
+          executable = {
+            command = lib.getExe' inputs.rustacean.packages.${pkgs.stdenv.hostPlatform.system}.codelldb "codelldb";
+            args = [ "--port" "65534" ];
+          };
         };
         server.default_settings = {
           rust-analyzer.files.excludeDirs = [ ".direnv" ];
