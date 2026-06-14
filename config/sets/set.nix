83d1f6bf872f951c0389bae064103d9a4644a56e
reesilva <reesilva@proton.me>
2026-06-14 04:00:57 +0200

    feat: neovide

--- config/sets/set.nix
+++ config/sets/set.nix
@@ -135,22 +135,17 @@
     };
 
     extraConfigLua = ''
-      local opt = vim.opt
-      local g = vim.g
-      local o = vim.o
-
       -- Neovide
-      if g.neovide then
-        g.neovide_fullscreen = false
-        g.neovide_hide_mouse_when_typing = false
-        g.neovide_refresh_rate = 165
-        g.neovide_cursor_vfx_mode = "ripple"
-        g.neovide_cursor_animate_command_line = true
-        g.neovide_cursor_animate_in_insert_mode = true
-        g.neovide_cursor_vfx_particle_lifetime = 5.0
-        g.neovide_cursor_vfx_particle_density = 14.0
-        g.neovide_cursor_vfx_particle_speed = 12.0
-        g.neovide_transparency = 0.9
+      if vim.g.neovide then
+        vim.g.neovide_fullscreen = false
+        vim.g.neovide_hide_mouse_when_typing = false
+        vim.g.neovide_refresh_rate = 165
+        vim.g.neovide_cursor_vfx_mode = "ripple"
+        vim.g.neovide_cursor_animate_command_line = true
+        vim.g.neovide_cursor_animate_in_insert_mode = true
+        vim.g.neovide_cursor_vfx_particle_lifetime = 5.0
+        vim.g.neovide_cursor_vfx_particle_density = 8.0
+        vim.g.neovide_cursor_vfx_particle_speed = 5.0
       end
     '';
   };
